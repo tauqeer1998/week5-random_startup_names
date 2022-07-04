7c552c34529f8031744481ea30f7ebc3c1cf8c5d
Muhammad Tauqeer <50796565+tauqeer1998@users.noreply.github.com>
2022-07-04 14:01:44 +0500

    Add files via upload

---
+++ lib/fav_list.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+
+class FavoriteList extends StatefulWidget {
+  final Set<String> data;
+  final Function savePrefs;
+  final TextStyle biggerFont;
+  FavoriteList({ Key? key,  required this.data,  required this.savePrefs, required this.biggerFont})
+      : super(key: key);
+
+  @override
+  _FavoriteListState createState() => _FavoriteListState();
+}
+
+class _FavoriteListState extends State<FavoriteList> {
+  late Set<String> _saved;
+
+  @override
+  void initState() {
+    _saved = widget.data;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Iterable<ListTile> tiles = _saved.map(
+          (String word) {
+        return ListTile(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                word,
+                style: widget.biggerFont,
+              ),
+              IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () {
+                  setState(() {
+                    _saved.remove(word);
+                    widget.savePrefs(_saved);
+                  });
+                },
+              )
+            ],
+          ),
+        );
+      },
+    );
+
+    final List<Widget> divided = ListTile.divideTiles(
+      context: context,
+      tiles: tiles,
+    ).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Saved Suggestions'),
+      ),
+      body: ListView(children: divided),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.pink[400],
+      ),
+      home: RandomWords(),
+    );
+  }
+}
